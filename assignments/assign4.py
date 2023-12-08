1840b8bc36767993e7c75e9429ba8001a37b466a
Calzy <ravenspear460@gmail.com>
2023-12-07 22:37:44 -0500

    fixing the dictionary issue

--- assignments/assign4.py
+++ assignments/assign4.py
@@ -32,4 +32,51 @@ class Product:
 
 class Inventory:
     def __init__(self):
-        self._inventory_data
+        self._inventory_data = {}
+    
+    def add_to_productInventory(self, productName, productPrice, productQuantity):
+        self._inventory_data[productName] = {
+            'price': productPrice,
+            'quantity': productQuantity
+        }
+    
+    def add_productQuantity(self, nameProduct, addQuantity):
+        if nameProduct in self._inventory_data:
+            self._inventory_data[nameProduct]['quantity'] += addQuantity
+    
+    def remove_productQuantity(self, nameProduct, removeQuantity):
+        if nameProduct in self._inventory_data:
+            currentQuantity = self._inventory_data[nameProduct]['quantity']
+            if currentQuantity >= removeQuantity:
+                self._inventory_data[nameProduct]['quantity'] -= removeQuantity
+
+    def get_productPrice(self, nameProduct):
+        return self._inventory_data[nameProduct]['price']
+    
+    def get_productQuantity(self, nameProduct):
+        return self._inventory_data[nameProduct]['quantity']
+
+    def display_Inventory(self):
+        for productName, productInfo in self._inventory_data.items():
+            print(f"{productName}, {productInfo['price']}, {productInfo['quantity']}")
+
+def main(fileName):
+    temp_invent = {}
+
+    try:
+        with open(fileName, 'r') as inventory:
+            for line in inventory:
+                itemName, itemPrice, itemQuantity, itemCategory = line.strip().split(',')                
+
+                temp_invent['item'].append(itemName)
+                temp_invent['price'].append(itemPrice)
+                temp_invent['quantity'].append(itemQuantity)
+                temp_invent['category'].append(itemCategory)
+
+            print(temp_invent)
+
+    except IOError:
+        # Handle the case where the file cannot be opened
+        print(f"Could not open file {fileName}")
+        
+main('inventory.csv')
